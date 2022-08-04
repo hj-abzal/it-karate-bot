635003a3f42485e5ca2a22012031508e607649f6
Abzal <abzal008suan@gmail.com>
2022-08-05 02:46:26 +0600

    .env

--- src/ikb-1-main/config.ts
+++ src/ikb-1-main/config.ts
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const USER_NAME = process.env.MONGO_DB_USER_NAME || "suan";
-const PASSWORD = process.env.MONGO_DB_USER_PASSWORD || "7777";
-const MONGO_DB_URL = process.env.MONGO_DB_URL || "MONGO_DB_URL=cluster0.olxw6.mongodb.net/";
+const PASSWORD = process.env.MONGO_DB_USER_PASSWORD || 7777;
+const MONGO_DB_URL = process.env.MONGO_DB_URL || "MONGO_DB_URL=cluster0.olxw6.mongodb.net";
 
 export const MongoDBUris = `mongodb+srv://${USER_NAME}:${PASSWORD}@${MONGO_DB_URL}?retryWrites=true&w=majority`;
 export const TelegramToken = process.env.TOKEN as string || "5530425128:AAFBK5i5Xmo_667o3GE5J5gFCgIaOvWjseo"
