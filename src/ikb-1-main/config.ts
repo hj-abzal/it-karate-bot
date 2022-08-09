a6e1e36c281a6a2e5a11abb4b66fcaeef8d74e14
Abzal <abzal008suan@gmail.com>
2022-08-09 18:48:21 +0600

    add dev testing vars

--- src/ikb-1-main/config.ts
+++ src/ikb-1-main/config.ts
@@ -2,11 +2,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const USER_NAME = process.env.MONGO_DB_USER_NAME || "suan";
-const PASSWORD = process.env.MONGO_DB_USER_PASSWORD || 7777;
-const MONGO_DB_URL = process.env.MONGO_DB_URL || "cluster0.olxw6.mongodb.net";
+const USER_NAME = process.env.MONGO_DB_USER_NAME || "test";
+const PASSWORD = process.env.MONGO_DB_USER_PASSWORD || "qwerty123";
+const MONGO_DB_URL = process.env.MONGO_DB_URL || "testdb.vn25fdl.mongodb.net";
 
 export const MongoDBUris = `mongodb+srv://${USER_NAME}:${PASSWORD}@${MONGO_DB_URL}?retryWrites=true&w=majority`;
-export const TelegramToken = process.env.TOKEN as string || "5530425128:AAFBK5i5Xmo_667o3GE5J5gFCgIaOvWjseo"
-export const PORT = process.env.PORT || 7542;
+export const TelegramToken = process.env.TOKEN as string || "5401157042:AAH8M6Z4wU44eYSBR29T06Oxg_HHLx3fQx0"
+export const PORT = process.env.PORT || 5000;
 export const adminLogin = "@SuanAbzal"
