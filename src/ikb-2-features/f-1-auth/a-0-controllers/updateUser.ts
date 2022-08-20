fee2bfaf4d76f8230d07f0bbe2541050de3da58f
Abzal <abzal008suan@gmail.com>
2022-08-21 04:34:25 +0600

    fix

--- src/ikb-2-features/f-1-auth/a-0-controllers/updateUser.ts
+++ src/ikb-2-features/f-1-auth/a-0-controllers/updateUser.ts
@@ -1,41 +1,49 @@
 import {Request, Response} from 'express';
-import User from '../a-2-models/user';
+import User, {IUser} from '../a-2-models/user';
 import Lesson from '../../f-2-lessons/l-2-models/lessons';
 import {sendMessage} from '../../f-3-message/m-3-helpers/sendMessage';
 
 export const updateUser = async (req: Request, res: Response) => {
-    const { _id, ...user} = req.body;
+    const {_id, ...user} = req.body;
 
     try {
         if (user?.level) {
-            const lesson = await Lesson.findOne({lessonOrder: user.level})
+            const lesson = await Lesson.findOne({lessonOrder: user.level});
             if (!lesson) {
                 res.status(404).json({
-                    error: "There is no such lesson in DB",
+                    error: 'There is no such lesson in DB',
                     body: req.body,
-                    in: "Lesson.findOne"
+                    in: 'Lesson.findOne'
                 });
                 return;
             }
-        };
-        const updatedUser = await User.findByIdAndUpdate(
+        }
+        ;
+        const updatedUser: IUser | null = await User.findByIdAndUpdate(
             _id,
             {...user},
             {new: true}
         ).exec();
-        if (updatedUser && updatedUser.chatID && user.level) {
+
+        if (updatedUser) {
             await sendMessage(
-                updatedUser.chatID,
+                updatedUser.uuid,
                 `Привет самурай, тебе дали доступ на ${user.level} урок!`
-            )
+            );
+            res.status(200).json({updatedUser});
+        } else {
+            res.status(500).json({
+                error: 'not updated?',
+                in: 'updatedUser'
+            });
         }
-        res.status(200).json({updatedUser});
+
 
     } catch (e) {
         res.status(500).json({
-            error: "Back doesn't know what the error is",
+            error: 'Back doesn\'t know what the error is',
             body: req.body,
-            in: "updatedUser"
+            in: 'updatedUser'
         });
     }
 };
